126a07f310756a860e6428e315b89695e6c15ff6
Marius van Witzenburg <info@mariusvw.com>
2017-04-19 20:50:03 +0200

    Header and indenting correction

--- public/index.php
+++ public/index.php
@@ -1,8 +1,9 @@
-<DOCUMENT html>
-    <head>
-        <title>Projects</title>
-    </head>
-    <body>
-        <?php foreach (glob('*', GLOB_ONLYDIR) as $project) { echo (preg_match('/^[a-z0-9\-]+$/', $project)) ? '<a href="' . $project . '">' . $project . '</a><br>' : ''; } ?>
-    </body>
+<!doctype html>
+<html>
+  <head>
+    <title>Projects</title>
+  </head>
+  <body>
+    <?php foreach (glob('*', GLOB_ONLYDIR) as $project) { echo (preg_match('/^[a-z0-9\-]+$/', $project)) ? '<a href="' . $project . '">' . $project . '</a><br>' : ''; } ?>
+  </body>
 </html>
